3d6909991a15fa012b21859e86169af5a878bb5b
Jimmy_승현수 <hsseung@maxst.com>
2023-04-23 01:42:33 +0900

    UpdateCouopnService 에 연결

--- src/coupon_service/application/service/update.coupon.service.ts
+++ src/coupon_service/application/service/update.coupon.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { UpdateCouponUseCase } from '../port/in/update.coupon.command';
 import { UpdateCouponCommand } from '../dto/update.coupon.command';
+import { Coupon } from 'src/coupon_service/domain/coupon/coupon.entity';
+import { UpdateCouponProperties } from 'src/coupon_service/domain/coupon/dto/update.coupon.properties';
 
 @Injectable()
 export class UpdateCouponService implements UpdateCouponUseCase {
   //todo db 연결
 
   async updateCoupon(command: UpdateCouponCommand): Promise<void> {
-    return;
+    const Coupon: Coupon = null; //todo db 에서 findId 를 통해서 받아오는것 필요
+    Coupon.updateCoupon(new UpdateCouponProperties(command));
   }
 }
