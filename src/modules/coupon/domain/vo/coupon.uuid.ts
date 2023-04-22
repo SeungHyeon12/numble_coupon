77f4ad0d404484b3f60ef5877b5a4104bd2cb357
Jimmy_승현수 <hsseung@maxst.com>
2023-04-22 13:21:59 +0900

    uuidGenerator Function 생성 및 CouponUuid Vo Constructor 에 적용

--- src/modules/coupon/domain/vo/coupon.uuid.ts
+++ src/modules/coupon/domain/vo/coupon.uuid.ts
@@ -1,3 +1,10 @@
+import uuidGenerator from 'src/common/function/uuid.generate.function';
+
 export class CouponUuid {
   uuid: string;
+
+  constructor(uuid: string | null) {
+    this.uuid = uuid;
+    if (!uuid) this.uuid = uuidGenerator();
+  }
 }
