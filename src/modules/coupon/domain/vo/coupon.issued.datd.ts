a6b1140cbfeba01e6e893cfa133c975b94c34efa
Jimmy_승현수 <hsseung@maxst.com>
2023-04-22 13:27:29 +0900

    couponIssuedDate 생성자 작성

--- src/modules/coupon/domain/vo/coupon.issued.datd.ts
+++ src/modules/coupon/domain/vo/coupon.issued.datd.ts
@@ -3,4 +3,9 @@ export class couponIssuedDate {
   private readonly couponIssuedStartDate: Date;
   // 쿠폰 발급일자 + 6
   private readonly couponIssuedEndDate: Date;
+
+  constructor(issuedStartDate: Date, couponIssuedEndDate: Date) {
+    this.couponIssuedStartDate = issuedStartDate;
+    this.couponIssuedEndDate = couponIssuedEndDate;
+  }
 }
