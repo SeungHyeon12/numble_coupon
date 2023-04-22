dd1eefe7b8ca02e23f883d348a14179ff2ca214d
Jimmy_승현수 <hsseung@maxst.com>
2023-04-22 12:35:48 +0900

    coupon의 도메인 프로퍼티들 정의

---
+++ src/modules/coupon/domain/coupon.entity.ts
@@ -0,0 +1,18 @@
+export class Coupon {
+  // coupon 의 id
+  private readonly couponId: string;
+
+  private readonly couponUuid: string;
+
+  private readonly couponDate: any;
+
+  private readonly couponDiscountInfo: any;
+
+  private couponCount: string;
+
+  private createdAt: Date;
+
+  private updatedAt: Date;
+
+  private deletedAt: Date;
+}
