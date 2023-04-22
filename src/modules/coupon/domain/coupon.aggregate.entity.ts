47ec46eda054f3cc07d97f6d7808d387ff6b0048
Jimmy_승현수 <hsseung@maxst.com>
2023-04-22 13:08:26 +0900

    couponIssuedDate vo 작성 및 적용

--- src/modules/coupon/domain/coupon.aggregate.entity.ts
+++ src/modules/coupon/domain/coupon.aggregate.entity.ts
@@ -2,6 +2,7 @@ import { BaseDomainEntity } from 'src/common/base.domain.entity';
 import { CouponValidity } from './coupon.validity.entity';
 import { CouponDiscountInfo } from './vo/coupon.disount.info';
 import { CouponUuid } from './vo/coupon.uuid';
+import { couponIssuedDate } from './vo/coupon.issued.datd';
 
 export class CouponAggregate extends BaseDomainEntity {
   // coupon 의 id
@@ -12,6 +13,9 @@ export class CouponAggregate extends BaseDomainEntity {
 
   // coupon 의 discount 관련 (vo)
   private readonly couponDiscountInfo: CouponDiscountInfo;
+
+  //실제 쿠폰을 발급받은 일자(vo)
+  private readonly couponIssuedDate: couponIssuedDate;
 
   // coupon 의 유효성에 대한 조건을 가지고 있는 validity(entity)
   private readonly couponValidity: CouponValidity;
---
+++ src/modules/coupon/domain/vo/coupon.issued.datd.ts
@@ -0,0 +1,6 @@
+export class couponIssuedDate {
+  // 쿠폰 발급일자
+  private readonly couponIssuedDate: Date;
+  // 쿠폰 발급일자 + 6
+  private readonly couponIssuedEndDate: Date;
+}
