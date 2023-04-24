d609c9804a042ae194e75ede3ca02f01bcf9ee96
Jimmy_승현수 <hsseung@maxst.com>
2023-04-24 21:28:32 +0900

    useCoupon 관련 로직 추가

--- src/coupon_service/application/service/use.coupon.service.ts
+++ src/coupon_service/application/service/use.coupon.service.ts
@@ -2,9 +2,13 @@ import { IssueCouponDomainService } from 'src/coupon_service/domain/coupon.issur
 import { UseCouponCommand } from '../dto/command/use.coupon.command';
 import { UseCouponUseCase } from '../port/in/usecase/use.coupon.usecase';
 import { CouponIssurance } from 'src/coupon_service/domain/coupon.issurance/coupon.issurance.entity';
+import { Inject } from '@nestjs/common';
+import { IssuranceStoreOutPort } from '../port/out/issurance.store.outport ';
 
 export class UseCouponService implements UseCouponUseCase {
   constructor(
+    @Inject('ISSURANCE_STORE_OUTPORT')
+    private readonly issuranceStoreAdapter: IssuranceStoreOutPort,
     private readonly issueCouponDomainService: IssueCouponDomainService,
   ) {}
 
@@ -15,5 +19,6 @@ export class UseCouponService implements UseCouponUseCase {
       command.useRequestDate,
     );
     issurance.useCoupon(command.productUuid);
+    this.issuranceStoreAdapter.update(issurance);
   }
 }
