7802864edef6c0d64fe101bfe4d1a61420064106
Jimmy_승현수 <hsseung@maxst.com>
2023-04-28 17:43:10 +0900

    logic 수정

--- src/coupon_service/application/service/issue.coupon.service.ts
+++ src/coupon_service/application/service/issue.coupon.service.ts
@@ -27,6 +27,7 @@ export class IssueCouponService implements IssueCouponUsecase {
     const coupon = await this.couponReaderAdaptor.getByCouponUuid(
       command.couonUuid,
     );
+    if (!coupon) throw new ConflictException('해당하는 쿠폰이 없습니다');
     const latestCouponIssurance =
       await this.issuranceReaderAdaptor.getIssuranceByIssuerUuidAndCouponUuid(
         command.issuerUuid,
