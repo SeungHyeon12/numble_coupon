90686b440f4c2d229f8ddca8e773b8ea61d07d64
Jimmy_승현수 <hsseung@maxst.com>
2023-04-29 00:16:44 +0900

    protobuf 추가

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { join } from 'path';
+import { MicroserviceOptions } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      options: {
+        package: 'couopon',
+        protoPath: join(__dirname, 'coupon_service/coupon.proto'),
+      },
+    },
+  );
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen();
 }
 bootstrap();
